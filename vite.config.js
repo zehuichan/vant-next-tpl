85f3ee057d0b55a6e85666bbd482f04d68338b08
jasconchan <jasonchenzehui@gmail.com>
2021-09-02 22:50:33 +0800

    d

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig((command, mode) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  console.log(root, env)
+  return {
+    plugins: [vue()]
+  }
 })
