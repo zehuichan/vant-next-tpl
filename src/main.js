302d0fa448be48ff1e96186709d5e7540a961fd5
chenzehui <jasonchenzehui@gmail.com>
2021-10-12 15:12:16 +0800

    d

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 
 import 'vant/lib/index.css';
 import './assets/less/index.less'
-import './theme/light.less'
 import './theme/dark.less'
 import vant from 'vant'
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
+import { Toast, Dialog } from 'vant'
 </script>
 
 <style lang="less">
