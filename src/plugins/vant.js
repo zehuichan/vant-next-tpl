54b3b74a6bc0ee884c316182ee2f0a0967d8c5cf
zehuichan <jasonchenzehui@gmail.com>
2022-01-10 10:40:38 +0800

    d

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,6 +1,8 @@
-import vant from 'vant'
-import 'vant/lib/index.css'
+import { Lazyload } from 'vant'
+import 'vant/es/toast/style/index'
+import 'vant/es/dialog/style/index'
+import 'vant/es/notify/style/index'
 
 export function setupVant(app) {
-  app.use(vant)
+  app.use(Lazyload)
 }
